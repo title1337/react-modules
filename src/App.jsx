a6eb50458a76d5d25249d51ff4f30639fd2b4b4f
Tle <title1337@gmail.com>
2026-09-25 19:33:21 +0700

    Fix Situation 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 import { Header } from './component/Header';
-import { MainContent } from './component/Main';
+import { MainContent } from './component/MainContent';
 import { ProductSection } from './component/ProductSection';
 import { FAQ } from './component/Faq';
 import { Footer } from './component/Footer';
--- src/component/Faq.jsx
+++ src/component/Faq.jsx
@@ -1,116 +1,61 @@
+'use client';
+
+import { Accordion, Box, Span } from '@chakra-ui/react';
+import { useState } from 'react';
 import Button from './common/Button';
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
-} from '@chakra-ui/react';
+
+const faqItems = [
+  {
+    value: 'a',
+    title: "Q: What makes XYZ Company's products so unique?",
+    text: "A: At XYZ Company, we pride ourselves on innovation and cutting-edge technology. Our products are crafted with a perfect blend of creativity and functionality, setting them apart from the competition. We constantly strive to push the boundaries of what's possible to deliver a truly exceptional user experience.",
+  },
+  {
+    value: 'b',
+    title:
+      'Q: How does XYZ Company contribute to sustainability and environmental conservation?',
+    text: 'A: XYZ Company is committed to environmental responsibility. We implement eco-friendly practices throughout our production process, from sourcing materials to manufacturing. Our dedication to sustainability extends beyond our products, as we actively engage in initiatives to reduce our carbon footprint and promote a greener future.',
+  },
+  {
+    value: 'c',
+    title:
+      "Q: What kind of support can customers expect from XYZ Company's customer service team?",
+    text: 'A: At XYZ Company, customer satisfaction is our top priority. Our dedicated support team is available 24/7 to address any inquiries or concerns you may have. Whether you need assistance with product usage, troubleshooting, or general information, our knowledgeable and friendly customer service representatives are always ready to provide prompt and effective support.',
+  },
+  {
+    value: 'd',
+    title: 'Q: How does XYZ Company stay ahead of industry trends?',
+    text: 'A: XYZ Company stays at the forefront of industry trends through continuous research and development. We invest heavily in staying ahead of the curve, monitoring market dynamics, and actively seeking feedback from our customers. By embracing innovation and staying agile, we ensure that our products and services evolve to meet the ever-changing needs of our dynamic customer base.',
+  },
+  {
+    value: 'e',
+    title:
+      "Q: Can you tell us about XYZ Company's commitment to community involvement?",
+    text: 'A: XYZ Company is deeply committed to giving back to the communities we serve. We actively engage in philanthropic initiatives, supporting local charities and community projects. Additionally, we encourage our employees to participate in volunteer programs, fostering a culture of social responsibility. By being actively involved in the community, we aim to make a positive impact beyond the products and services we offer.',
+  },
+];
 
 export function FAQ() {
+  const [value, setValue] = useState(['a']);
+
   return (
     <section id="faq">
       <h1>FAQ (คำถามที่พบบ่อย)</h1>
-      <Accordion defaultIndex={0}>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Q: What makes XYZ Company's products so unique?
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            A: At XYZ Company, we pride ourselves on innovation and cutting-edge
-            technology. Our products are crafted with a perfect blend of
-            creativity and functionality, setting them apart from the
-            competition. We constantly strive to push the boundaries of what's
-            possible to deliver a truly exceptional user experience.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Q: How does XYZ Company contribute to sustainability and
-                environmental conservation?
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            A: XYZ Company is committed to environmental responsibility. We
-            implement eco-friendly practices throughout our production process,
-            from sourcing materials to manufacturing. Our dedication to
-            sustainability extends beyond our products, as we actively engage in
-            initiatives to reduce our carbon footprint and promote a greener
-            future.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Q: What kind of support can customers expect from XYZ Company's
-                customer service team?
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            A: At XYZ Company, customer satisfaction is our top priority. Our
-            dedicated support team is available 24/7 to address any inquiries or
-            concerns you may have. Whether you need assistance with product
-            usage, troubleshooting, or general information, our knowledgeable
-            and friendly customer service representatives are always ready to
-            provide prompt and effective support.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Q: How does XYZ Company stay ahead of industry trends?
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            A: XYZ Company stays at the forefront of industry trends through
-            continuous research and development. We invest heavily in staying
-            ahead of the curve, monitoring market dynamics, and actively seeking
-            feedback from our customers. By embracing innovation and staying
-            agile, we ensure that our products and services evolve to meet the
-            ever-changing needs of our dynamic customer base.
-          </AccordionPanel>
-        </AccordionItem>
-
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
-                Q: Can you tell us about XYZ Company's commitment to community
-                involvement?
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            A: XYZ Company is deeply committed to giving back to the communities
-            we serve. We actively engage in philanthropic initiatives,
-            supporting local charities and community projects. Additionally, we
-            encourage our employees to participate in volunteer programs,
-            fostering a culture of social responsibility. By being actively
-            involved in the community, we aim to make a positive impact beyond
-            the products and services we offer.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      <Accordion.Root value={value} onValueChange={(e) => setValue(e.value)}>
+        {faqItems.map((item) => (
+          <Accordion.Item key={item.value} value={item.value}>
+            <Accordion.ItemTrigger>
+              <Span flex="1" textAlign="left">
+                {item.title}
+              </Span>
+              <Accordion.ItemIndicator />
+            </Accordion.ItemTrigger>
+            <Accordion.ItemContent>
+              <Accordion.ItemBody pb={4}>{item.text}</Accordion.ItemBody>
+            </Accordion.ItemContent>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
     </section>
   );
 }
